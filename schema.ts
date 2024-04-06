a0aa188f1e142c82cd8278d281e369337c220e62
Yu <114864805+redpandaQAQ@users.noreply.github.com>
2024-04-06 22:33:29 +0000

    Refactor schema.ts to use more descriptive names for types and properties

--- schema.ts
+++ schema.ts
@@ -2,44 +2,46 @@
 
 export interface Formula {
     type: "formula";
-    result: (validFormula | UnknownText)[];
+    result: MathFunction | CellAddress | CellArea | NumberValue | StringValue;
 }
 
 // Use this type for order items that match nothing else
-export interface UnknownText {
+
+/*export interface UnknownText {
     type: "unknown",
     text: string; // The text that wasn't understood
 }
+*/
 
-export type validFormula = Functions | CellRef | CellAreaRef | SNumber | SString;
+export type validFormula = MathFunction | CellAddress | CellArea | NumberValue | StringValue;
 
-export interface Functions {
-    type: "funcName";
-    name: "ADD " | "MUL" | "SUB" | "DIV" | "MOD" | "POW" | "SQRT" | "ABS" | "EXP" | "LOG" | "LOG10" | "LOG2" | "SUM" | "AVG";
-    argsNumber: number;
-    args: validFormula[];
+export interface MathFunction {//In my understanding, the naming is relevant to the result. if you simply name this "Function", gpt might thought cell address as function, so I named it MathFunction, we may try other names later
+    type: "MathFunction";
+    functionName: "ADD" | "MUL" | "SUB" | "DIV" | "MOD" | "POW" | "SQRT" | "ABS" | "EXP" | "LOG" | "LOG10" | "LOG2" | "SUM" | "AVG";
+    //argsNumber: number;
+    functionArgs: validFormula[];
 }
 
-export interface CellRef {
-    type: "cellref";
+export interface CellAddress {
+    type: "CellAddress";
     column: "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z";
     row: number;
 
 }
 
-export interface CellAreaRef {
-    type: "cellarearef";
-    start: CellRef;
-    end: CellRef;
+export interface CellArea {
+    type: "CellArea";
+    startCellAddress: CellAddress;
+    endCellAddress: CellAddress;
 
 }
 
-export interface SNumber {
-    type: "number";
+export interface NumberValue {
+    type: "NumberValue";
     value: number;
 }
 
-export interface SString {
-    type: "string";
+export interface StringValue {
+    type: "StringValue";
     value: string;
 }
