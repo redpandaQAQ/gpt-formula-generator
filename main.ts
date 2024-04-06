3e4eaa14d770b8d94518ffd1487da488f27a7f09
Yu <114864805+redpandaQAQ@users.noreply.github.com>
2024-04-06 22:33:44 +0000

    Refactor main.ts to process formulas and add myProcessRequests function

--- main.ts
+++ main.ts
@@ -6,7 +6,9 @@ import path from "path";
 import { createJsonTranslator, createLanguageModel } from "typechat";
 import { createTypeScriptJsonValidator } from "typechat/ts";
 import { processRequests } from "typechat/interactive";
-import { Formula } from "./schema";
+import { Formula, validFormula } from "./schema";
+import { myProcessRequests } from "./myProcessRequests";
+import { json } from "stream/consumers";
 
 const dotEnvPath = findConfig(".env");
 assert(dotEnvPath, ".env file not found!");
@@ -17,13 +19,36 @@ const schema = fs.readFileSync(path.join(__dirname, "schema.ts"), "utf8");
 const validator = createTypeScriptJsonValidator<Formula>(schema, "Formula");
 const translator = createJsonTranslator(model, validator);
 
-function processOrder(formula: Formula) {
+function processFormula(formula: validFormula):string {
     // Process the items in the cart
-    void formula;
+    let result:string = "";
+    if(formula.type === "MathFunction") {
+        result = (formula.functionName + "(");
+        for(let i = 0; i < formula.functionArgs.length; i++) {
+            result += processFormula(formula.functionArgs[i]);
+            if(i < formula.functionArgs.length - 1) {
+                result += ",";  
+            }
+        }
+        result += ")";
+    }
+    else if(formula.type === "CellAddress") {
+        result = (formula.column + formula.row.toString());
+    }
+    else if(formula.type === "CellArea") {
+        result = processFormula(formula.startCellAddress) + ":" + processFormula(formula.endCellAddress);
+    }
+    else if(formula.type === "NumberValue") {
+        result = formula.value.toString();
+    }
+    else if(formula.type === "StringValue") {
+        result = formula.value;
+    }
+    return result;
 }
 
 // Process requests interactively or from the input file specified on the command line
-processRequests("☕> ", process.argv[2], async (request) => {
+myProcessRequests("Discribe the formula: ", process.argv[2], async (request) => {
     const response = await translator.translate(request);
     if (!response.success) {
         console.log(response.message);
@@ -31,13 +56,14 @@ processRequests("☕> ", process.argv[2], async (request) => {
     }
     const formula = response.data;
     console.log(JSON.stringify(formula, undefined, 2));
-    if (formula.result.some(formula => formula.type === "unknown")) {
+    let result = formula.result;
+    /*
+    if (formula.result.type !== "validFormula") {
         console.log("I didn't understand the following:");
-        for (const result of formula.result) {
-            if (result.type === "unknown") console.log(result.text);
-        }
+        if (result.type === "unknown") console.log(result.text);
         return;
     }
-    processOrder(formula);
+    */
+    console.log("=" + processFormula(result));
     console.log("Success!");
 });
